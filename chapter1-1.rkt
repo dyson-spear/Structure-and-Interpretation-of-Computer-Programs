83b122ea356e12a846c8acdcf92886717cf95448
dyson-spear <138142159+dyson-spear@users.noreply.github.com>
2023-07-18 16:32:03 -0700

    fix edge case in sum of greater squares

--- chapter1-1.rkt
+++ chapter1-1.rkt
@@ -49,8 +49,8 @@
 ;; Exercise 1.3 Define a procedure that takes three numbers as arguments and returns the sum of the squares of the two larger numbers.
 (display "Exercise 1.3: ")
 (define (larger-squares x y z)
-  (cond ((and (> x z) (> y z)) (sum-of-squares x y))
-        ((and (> x y) (> z y)) (sum-of-squares x z))
+  (cond ((and (>= x z) (>= y z)) (sum-of-squares x y))
+        ((and (>= x y) (>= z y)) (sum-of-squares x z))
         (else (sum-of-squares y z))))
 (define (sum-of-squares x y)
   (+ (square x) (square y)))
