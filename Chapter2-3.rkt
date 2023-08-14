fe93bc2dbb807d41ff5d771672b984baedd527bf
dyson-spear <138142159+dyson-spear@users.noreply.github.com>
2023-08-14 00:24:00 -0700

    add exercise 2.59

--- Chapter2-3.rkt
+++ Chapter2-3.rkt
@@ -108,4 +108,31 @@
 (deriv expr1 'x) ;(+ (* 3 (* 2 x)) 6)
 ;;Exercise 2.57 and 2.58 - skipped
 
+;;Exercise 2.59 - Implement union-set
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)        
+         (cons (car set1)
+               (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
 
+(define (union-set set1 set2)
+  (cond
+    ((null? set1) set2)
+    ((null? set2) set1)
+    ((element-of-set? (car set1) set2)
+      (union-set (cdr set1) set2))
+    (else (adjoin-set (car set1) set2))
+    ))
+(define set1 (list 1 2 3 4))
+(define set2 (list 3 4 5 6))
+(union-set set1 set2)
+(intersection-set set1 set2)
